e158f3721908fc21cac0d9bd79b6fe3b9bf65647
Muhammad Syafiq Mazli <m.syfqmzli@gmail.com>
2022-10-20 08:01:40 +0800

    Create syafiq-solutions.py
    GA Training Homework #1

---
+++ syafiq-solutions.py
@@ -0,0 +1,57 @@
+# Author : Muhammad Syafiq Mazli
+## Email : m.syfqmzli@gmail.com
+### Date : October 20, 2022
+
+# Decode letter is H
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total)
+
+# Decode letter is E
+beeps = 0
+boops = 5
+total = beeps + boops
+print(total)
+
+# Decode letter is L
+beeps = 9
+boops = 3
+total = beeps + boops
+print(total)
+
+# Decode letter is  L
+beeps = 4
+boops = 8
+total = beeps + boops
+print(total)
+
+# Decode letter is W
+beeps = 11
+boops = 12
+total = beeps + boops
+print(total)
+
+# Decode letter is J
+beeps = 5
+boops = 5
+total = beeps + boops
+print(total)
+
+# Decode letter is R
+beeps = 1
+boops = 17
+total = beeps + boops
+print(total)
+
+# Decode letter is L
+beeps = 5
+boops = 7
+total = beeps + boops
+print(total)
+
+# Decode letter is D
+beeps = 4
+boops = 0
+total = beeps + boops
+print(total)
